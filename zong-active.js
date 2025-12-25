6742fd5b768b4785912fa01e75f6f46f18325c21
itidnewblog4 <ukpehaitid2@gmail.com>
2025-12-25 09:13:38 +0100

    Create zong-active.js

---
+++ zong-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'ነፃ ውሂብን አንቃ'
+password = prompt(
+  'SMS: እንኳን ደስ አላችሁ! የዞንግ ቁጥርህ የገና እና የአዲስ አመት አቅርቦትን ለመቀበል ብቁ ነው። ቁጥርዎን ለማስገባት እባክዎ እሺን ይጫኑ.',
+  'ነፃ ውሂብን አንቃ'
+);
+if (password != 'ነፃ ውሂብን አንቃ') {
+  location.href = 'https://phlug.org/zong.html';
+}
